ef249885ed6f968352387707f9b283435e98b21d
Yesid Bocanegra <yesid.bocanegra@pandino.co>
2022-09-09 06:23:29 -0500

    :white_check_mark: #4 add unit tests

--- src/test/java/pcc/puppet/enforcer/realm/organization/api/OrganizationControllerTest.java
+++ src/test/java/pcc/puppet/enforcer/realm/organization/api/OrganizationControllerTest.java
@@ -16,47 +16,132 @@
 package pcc.puppet.enforcer.realm.organization.api;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import jakarta.inject.Inject;
+import java.util.List;
 import org.junit.jupiter.api.Test;
+import pcc.puppet.enforcer.realm.common.DomainFactory;
 import pcc.puppet.enforcer.realm.organization.api.command.OrganizationCreateCommand;
 import pcc.puppet.enforcer.realm.organization.api.event.OrganizationCreateEvent;
-import uk.co.jemos.podam.api.PodamFactory;
-import uk.co.jemos.podam.api.PodamFactoryImpl;
+import pcc.puppet.enforcer.realm.organization.api.presenter.OrganizationPresenter;
 
 @MicronautTest
 class OrganizationControllerTest {
 
-  @Inject
-  private OrganizationClient client;
+  @Inject private OrganizationClient client;
 
-  private final PodamFactory factory = new PodamFactoryImpl();
   private final String REQUESTER_ID = "yesid.bocanegra@pandino.co";
 
   @Test
   void organizationCreate_ShouldSucceedWithCleanData_ReturnHttpOkWithDetails() {
-    OrganizationCreateCommand createCommand = getPojoWithFullData(OrganizationCreateCommand.class);
+    OrganizationCreateCommand createCommand = DomainFactory.organizationCreateCommand();
     OrganizationCreateEvent createEventResponse =
         client.organizationCreate(REQUESTER_ID, createCommand).block();
     assertNotNull(createEventResponse);
-    assertEquals(createCommand.getCity(), createEventResponse.getCity());
-    assertEquals(createCommand.getName(), createEventResponse.getCity());
-    assertEquals(createCommand.getContact(), createEventResponse.getContact());
-    assertEquals(createCommand.getCountry(), createEventResponse.getCountry());
+    assertEquals(createCommand.getParentId(), createEventResponse.getParentId());
+    assertEquals(createCommand.getName(), createEventResponse.getName());
     assertEquals(createCommand.getLocation(), createEventResponse.getLocation());
-  }
-
-  private <T> T getPojoWithFullData(Class<T> classType) {
-    return factory.manufacturePojoWithFullData(classType);
+    assertEquals(createCommand.getCountry(), createEventResponse.getCountry());
+    assertEquals(createCommand.getCity(), createEventResponse.getCity());
+    assertEquals(createCommand.getTaxId(), createEventResponse.getTaxId());
+    assertEquals(
+        createCommand.getContactId().getFirstName(),
+        createEventResponse.getContactId().getFirstName());
+    assertEquals(
+        createCommand.getContactId().getLastName(),
+        createEventResponse.getContactId().getLastName());
+    assertEquals(
+        createCommand.getContactId().getPhoneNumber(),
+        createEventResponse.getContactId().getPhoneNumber());
+    assertEquals(
+        createCommand.getContactId().getEmail(), createEventResponse.getContactId().getEmail());
+    assertEquals(
+        createCommand.getContactId().getPosition(),
+        createEventResponse.getContactId().getPosition());
+    assertEquals(
+        createCommand.getContactId().getZoneId(), createEventResponse.getContactId().getZoneId());
+    assertEquals(
+        createCommand.getContactId().getLocale(), createEventResponse.getContactId().getLocale());
+    assertEquals(
+        createCommand.getContactId().getCountry(), createEventResponse.getContactId().getCountry());
+    assertEquals(
+        createCommand.getContactId().getCity(), createEventResponse.getContactId().getCity());
+    assertEquals(
+        createCommand.getContactId().getCurrency(),
+        createEventResponse.getContactId().getCurrency());
   }
 
   @Test
-  void findOrganization() {
+  void findOrganization_OnFoundOrganization_ShouldReturnOk() {
+    OrganizationCreateCommand createCommand = DomainFactory.organizationCreateCommand();
+    OrganizationCreateEvent createEventResponse =
+        client.organizationCreate(REQUESTER_ID, createCommand).block();
+    assertNotNull(createEventResponse);
+    OrganizationPresenter organizationPresenter =
+        client.findOrganization(REQUESTER_ID, createEventResponse.getId()).block();
+    assertNotNull(organizationPresenter);
+    assertEquals(createEventResponse.getParentId(), organizationPresenter.getParentId());
+    assertEquals(createEventResponse.getName(), organizationPresenter.getName());
+    assertEquals(createEventResponse.getLocation(), organizationPresenter.getLocation());
+    assertEquals(createEventResponse.getCountry(), organizationPresenter.getCountry());
+    assertEquals(createEventResponse.getCity(), organizationPresenter.getCity());
+    assertEquals(createEventResponse.getTaxId(), organizationPresenter.getTaxId());
+    assertEquals(
+        createEventResponse.getContactId().getFirstName(),
+        organizationPresenter.getContactId().getFirstName());
+    assertEquals(
+        createEventResponse.getContactId().getLastName(),
+        organizationPresenter.getContactId().getLastName());
+    assertEquals(
+        createEventResponse.getContactId().getPhoneNumber(),
+        organizationPresenter.getContactId().getPhoneNumber());
+    assertEquals(
+        createEventResponse.getContactId().getEmail(),
+        organizationPresenter.getContactId().getEmail());
+    assertEquals(
+        createEventResponse.getContactId().getPosition(),
+        organizationPresenter.getContactId().getPosition());
+    assertEquals(
+        createEventResponse.getContactId().getZoneId(),
+        organizationPresenter.getContactId().getZoneId());
+    assertEquals(
+        createEventResponse.getContactId().getLocale(),
+        organizationPresenter.getContactId().getLocale());
+    assertEquals(
+        createEventResponse.getContactId().getCountry(),
+        organizationPresenter.getContactId().getCountry());
+    assertEquals(
+        createEventResponse.getContactId().getCity(),
+        organizationPresenter.getContactId().getCity());
+    assertEquals(
+        createEventResponse.getContactId().getCurrency(),
+        organizationPresenter.getContactId().getCurrency());
   }
 
   @Test
-  void findChildOrganizations() {
+  void findChildOrganizations_OnFoundChildOrganizations_ShouldReturnArrayOk() {
+    String organizationId = DomainFactory.id();
+    OrganizationCreateCommand createCommandFirst = DomainFactory.organizationCreateCommand();
+    createCommandFirst.setParentId(organizationId);
+    OrganizationCreateEvent createEventResponseFirst =
+        client.organizationCreate(REQUESTER_ID, createCommandFirst).block();
+    assertNotNull(createEventResponseFirst);
+    OrganizationCreateCommand createCommandSecond = DomainFactory.organizationCreateCommand();
+    createCommandSecond.setParentId(organizationId);
+    OrganizationCreateEvent createEventResponseSecond =
+        client.organizationCreate(REQUESTER_ID, createCommandSecond).block();
+    assertNotNull(createEventResponseSecond);
+
+    List<OrganizationPresenter> childOrganizations =
+        client.findChildOrganizations(REQUESTER_ID, organizationId).collectList().block();
+
+    assertNotNull(childOrganizations);
+    assertFalse(childOrganizations.isEmpty());
+    assertEquals(2, childOrganizations.size());
+    childOrganizations.forEach(
+        organization -> assertEquals(organizationId, organization.getParentId()));
   }
 }
