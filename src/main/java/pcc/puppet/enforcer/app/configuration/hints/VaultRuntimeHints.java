71552678056f38dc7fe7d55689dc648d18f66b9a
Yesid Bocanegra <yesid.bocanegra@pandino.co>
2023-07-21 16:36:13 -0500

    :art: #54: fix imports

--- src/main/java/pcc/puppet/enforcer/app/configuration/hints/VaultRuntimeHints.java
+++ src/main/java/pcc/puppet/enforcer/app/configuration/hints/VaultRuntimeHints.java
@@ -17,13 +17,13 @@
 package pcc.puppet.enforcer.app.configuration.hints;
 
 import org.springframework.aot.hint.MemberCategory;
+import org.springframework.aot.hint.RuntimeHints;
 import org.springframework.aot.hint.RuntimeHintsRegistrar;
 import org.springframework.aot.hint.TypeReference;
 
 public class VaultRuntimeHints implements RuntimeHintsRegistrar {
   @Override
-  public void registerHints(
-      org.springframework.aot.hint.RuntimeHints hints, ClassLoader classLoader) {
+  public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
     hints
         .reflection()
         .registerType(
